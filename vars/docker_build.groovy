acd206ab84167fad666ecc838b788e1af81bfd39
zioosad <kiocoder@outlook.com>
2025-02-02 13:04:29 -0700

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser) {
+    sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
